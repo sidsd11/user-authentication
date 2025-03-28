36855cae53a0744245b3eb394b1b528e27c5b2ea
Siddharth Deshetti <58861396+sidsd11@users.noreply.github.com>
2025-03-29 01:33:03 +0530

    Update mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () => 
         console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/Authentication`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/DBName`);
 }
 
 export default connectDB;
